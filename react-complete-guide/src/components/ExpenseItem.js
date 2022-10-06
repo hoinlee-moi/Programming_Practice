482bd5f31f707678f1c3e5bb61e27895610989a3
hoinlee-moi <lhi3048@gmail.com>
2022-10-06 20:15:06 +0900

    add : react 강의 추가

---
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Outober 6th 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$ 294.67 </div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
