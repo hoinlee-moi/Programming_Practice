8355c2d1f2f95a7354d9497f224388fedaa8eff5
hoinlee-moi <lhi3048@gmail.com>
2022-10-22 22:43:33 +0900

    modify : 띄어쓰기 수정

--- react-complete-guide/src/components/Expenses.js
+++ react-complete-guide/src/components/Expenses.js
@@ -3,7 +3,6 @@ import "../extra-files/Expenses.css"
 
 function Expenses(props) {
   
-
   return (
     <div className="expenses">
       <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
