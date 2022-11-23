929566e377ba5937f7c60ac986d16f1621130900
hoinlee-moi <lhi3048@gmail.com>
2022-11-23 23:13:01 +0900

    add: 컴포지션

---
+++ react-complete-guide/src/components/Card.js
@@ -0,0 +1,9 @@
+import "../extra-files/Card.css";
+
+function Card(props) {
+    const calsses = 'card ' + props.className;
+    
+  return <div className={calsses}>{props.children}</div>;
+}
+
+export default Card;
