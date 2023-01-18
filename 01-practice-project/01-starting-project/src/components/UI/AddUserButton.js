6440c695858c9c2e5ee0db16fb0de1272deb385d
hoinlee-moi <lhi3048@gmail.com>
2023-01-18 21:33:16 +0900

    add : useSstate 복습

--- 01-practice-project/01-starting-project/src/components/UI/AddUserButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import classes from "./AddUserButton.module.css"
-
-const AddUserButton = (props) => {
-    return <button type="submit" className={classes.button}>{props.children}</button>
-};
-
-export default AddUserButton;
