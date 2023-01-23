067b89bfb7a6cfeea2cb596e6bedafbe046f0033
hoinlee-moi <lhi3048@gmail.com>
2023-01-23 22:37:44 +0900

    add

--- 01-practice-project/01-starting-project/src/components/UI/Button.js
+++ 01-practice-project/01-starting-project/src/components/UI/Button.js
@@ -3,6 +3,7 @@ import React from "react";
 import classes from "./Button.module.css"
 
 const Butoon = (props) => {
+    
     return <button type={props.type || 'button'} className={classes.button}>{props.children}</button>
 };
 
--- 01-practice-project/01-starting-project/src/components/Users/User.js
+++ 01-practice-project/01-starting-project/src/components/Users/User.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const User = (props) => {};
 
 export default User;
