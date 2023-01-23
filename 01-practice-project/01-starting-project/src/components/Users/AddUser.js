478b85935e72131ef4eed04eafa23e997425fbb9
hoinlee-moi <lhi3048@gmail.com>
2023-01-24 02:49:01 +0900

    add : 검증 추가 및 로직 재설정

--- 01-practice-project/01-starting-project/src/components/Users/AddUser.js
+++ 01-practice-project/01-starting-project/src/components/Users/AddUser.js
@@ -11,7 +11,12 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
-
+    if(enteredUsername.trim().length===0 || enteredAge.trim().length===0){
+      return;
+    } 
+    if(+enteredAge < 1){
+      return
+    }
     setEnteredAge('')
     setEnteredUsername('')
   };
@@ -27,9 +32,9 @@ const AddUser = (props) => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} />
+        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
         <Butoon type="submit">Add User</Butoon>
       </form>
     </Card>
