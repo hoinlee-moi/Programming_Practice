28e27abb9c892d1e5bd0700d7bdeeb2eca17c738
hoinlee-moi <lhi3048@gmail.com>
2023-01-27 02:39:37 +0900

    add : 일기장 만들기 연습

--- inflearn-react-practice/src/App.js
+++ inflearn-react-practice/src/App.js
@@ -1,12 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import DiaryEditor from "./DiaryEditor";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>안녕 리액트</h2>
-      </header>
+      <DiaryEditor />
     </div>
   );
 }
