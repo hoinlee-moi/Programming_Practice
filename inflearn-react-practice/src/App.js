f91bce999fd51a3e55b8fc41128e548bccb857f2
hoinlee-moi <lhi3048@gmail.com>
2023-01-27 18:48:39 +0900

    add : dummyData를 통한 list 만들기

--- inflearn-react-practice/src/App.js
+++ inflearn-react-practice/src/App.js
@@ -1,10 +1,36 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "이호인",
+    content: "하이1",
+    emotion: 5,
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "홍길동",
+    content: "하이2",
+    emotion: 4,
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "임지지",
+    content: "하이3",
+    emotion: 1,
+    createdDate: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
