b5c250c46c25618146df5fffffc0d87d6cff0260
hoinlee-moi <lhi3048@gmail.com>
2023-01-29 04:16:02 +0900

    add:수정버튼추가

--- inflearn-react-practice/src/App.js
+++ inflearn-react-practice/src/App.js
@@ -45,16 +45,19 @@ function App() {
     setData([newItem, ...data]);
   };
 
-  const onDelete = (targetId) => {
+  const onRemove = (targetId) => {
     console.log(`${targetId}가 삭제되었습니다`);
     const newDiaryList = data.filter((item) => item.id !== targetId);
     setData(newDiaryList);
   };
 
+  const onEdit = (targetId, newContent) => {
+    setData(data.map((item) => (item.id === targetId ? { ...item, content: newContent } : item)));
+  };
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} onDelete={onDelete} />
+      <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
 }
--- inflearn-react-practice/src/DiaryList.js
+++ inflearn-react-practice/src/DiaryList.js
@@ -1,14 +1,13 @@
 import DiaryItem from "./DiaryItem";
 
-const DiaryList = ({ onDelete, diaryList }) => {
-  console.log(diaryList);
+const DiaryList = ({ onEdit, onRemove, diaryList }) => {
   return (
     <div className="DiaryList">
       <h2>일기 리스트</h2>
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
         {diaryList.map((item) => {
-          return <DiaryItem key={item.id} {...item} onDelete={onDelete} />;
+          return <DiaryItem key={item.id} {...item} onRemove={onRemove} onEdit={onEdit} />;
         })}
       </div>
     </div>
