4db968d54d772baba899091bf947a308a7f6828d
hoinlee-moi <lhi3048@gmail.com>
2023-01-29 16:50:18 +0900

    add : useMemo 사용해보기

--- inflearn-react-practice/src/App.js
+++ inflearn-react-practice/src/App.js
@@ -1,37 +1,31 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect, useMemo } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
-
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "이호인",
-//     content: "하이1",
-//     emotion: 5,
-//     createdDate: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: "홍길동",
-//     content: "하이2",
-//     emotion: 4,
-//     createdDate: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: "임지지",
-//     content: "하이3",
-//     emotion: 1,
-//     createdDate: new Date().getTime(),
-//   },
-// ];
 
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments").then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((item) => {
+      return {
+        author: item.email,
+        content: item.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const createdDate = new Date().getTime();
@@ -55,10 +49,26 @@ function App() {
   const onEdit = (targetId, newContent) => {
     setData(data.map((item) => (item.id === targetId ? { ...item, content: newContent } : item)));
   };
+
+  const getDiaryAnalysis = useMemo(() => {
+    console.log("일기 분석 시작");
+
+    const goodCount = data.filter((item) => item.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]);
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length} </div>
+      <div>기분 좋은 일기 개수 : {goodCount} </div>
+      <div>기분 나쁜 일기 개수 : {badCount} </div>
+      <div>기분 좋은 일기 비율 : {goodRatio} </div>
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
