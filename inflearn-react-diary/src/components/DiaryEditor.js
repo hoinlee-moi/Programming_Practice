ccf660df993922b5a327e9e866068d41533e42e2
hoinlee-moi <lhi3048@gmail.com>
2023-02-09 02:31:44 +0900

    add : 수정페이지 만들기

--- inflearn-react-diary/src/components/DiaryEditor.js
+++ inflearn-react-diary/src/components/DiaryEditor.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { DiaryDispatchContext } from "../App";
 
@@ -40,7 +40,7 @@ const getStringDate = (date) => {
   return dateOffset.toISOString().slice(0, 10);
 };
 
-const DiaryEditor = () => {
+const DiaryEditor = ({ isEdit, originData }) => {
   const navigate = useNavigate();
 
   const contentRef = useRef();
@@ -49,7 +49,7 @@ const DiaryEditor = () => {
   const [emotion, setEmotion] = useState(3);
   const [content, setContent] = useState("");
 
-  const { onCreate } = useContext(DiaryDispatchContext);
+  const { onCreate, onEdit } = useContext(DiaryDispatchContext);
 
   const handleClickEmote = (emotion) => {
     setEmotion(emotion);
@@ -60,18 +60,30 @@ const DiaryEditor = () => {
       contentRef.current.focus();
       return;
     }
-    onCreate(date, content, emotion);
+    if (window.confirm(isEdit ? "일기를 수정하시겠습니까?" : "새로운 일기를 작성하시겠습니까?")) {
+      if (!isEdit) onCreate(date, content, emotion);
+      else onEdit(originData.id, date, content, emotion);
+    }
+
     navigate("/", { replace: true });
   };
 
+  useEffect(() => {
+    if (isEdit) {
+      setDate(getStringDate(new Date(parseInt(originData.date))));
+      setEmotion(originData.emotion);
+      setContent(originData.content);
+    }
+  }, [isEdit, originData]);
+
   return (
     <div className="DiaryEditor">
-      <MyHeader headText={"새 일기쓰기"} leftChild={<Mybutton text={"< 뒤로가기"} onClick={() => navigate(-1)} />} />
+      <MyHeader headText={isEdit ? "일기 수정하기" : "새 일기쓰기"} leftChild={<Mybutton text={"< 뒤로가기"} onClick={() => navigate(-1)} />} />
       <div>
         <section>
           <h4>오늘은 언제인가요?</h4>
           <div className="input_box">
-            <input className="input_date" defaultValue={date} type="date" onChange={(e) => setDate(e.target.value)} />
+            <input className="input_date" value={date} type="date" onChange={(e) => setDate(e.target.value)} />
           </div>
         </section>
         <section>
--- inflearn-react-diary/src/pages/Edit.js
+++ inflearn-react-diary/src/pages/Edit.js
@@ -1,23 +1,23 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+import DiaryEditor from "../components/DiaryEditor";
 
 const Edit = () => {
+  const [originData, setOriginData] = useState();
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
+  const { id } = useParams();
+  const diaryList = useContext(DiaryStateContext);
 
-  const id = searchParams.get("id");
-  const mode = searchParams.get("mode");
-  console.log(id, mode);
-
-  return (
-    <div>
-      <h1>Edit</h1>
-      <p>이곳은 일기 수정페이지 입니다.</p>
-      <button onClick={() => setSearchParams({ who: "hoin" })}>QS 변경</button>
-      <button onClick={() => navigate("/home")}>Home으로 가기</button>
-      <button onClick={() => navigate(-1)}>뒤로가기</button>
-      <button onClick={() => navigate(1)}>앞으로가기</button>
-    </div>
-  );
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find((it) => parseInt(it.id) === parseInt(id));
+      if (targetDiary) {
+        setOriginData(targetDiary);
+      } else navigate("/", { replace: true });
+    }
+  }, [diaryList, id]);
+  return <div>{originData && <DiaryEditor isEdit={true} originData={originData} />}</div>;
 };
 
 export default Edit;
