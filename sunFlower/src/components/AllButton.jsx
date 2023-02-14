2edf23224d646ceb84d716a2456d0c0674bdd515
hoinlee-moi <lhi3048@gmail.com>
2023-02-15 00:55:45 +0900

    modify : 타입스크립트 템플릿사용해보기 위한 프로젝트 재설계

--- sunFlower/src/components/AllButton.jsx
+++
@@ -1,5 +0,0 @@
-const AllButton = (props) => {
-  return <button type={props.type}>{props.children}</button>;
-};
-
-export default AllButton;
--- sunFlower/src/components/User/UserLoginInput.jsx
+++
@@ -1,9 +0,0 @@
-const UserLoginInput = ({ value, type, placeHolder, userInfo }) => {
-  const inputValueHandler = (e) => {
-    userInfo(e.target.value, e.target.placeholder);
-  };
-
-  return <input value={value} type={type} placeholder={placeHolder} onChange={inputValueHandler} />;
-};
-
-export default UserLoginInput;
