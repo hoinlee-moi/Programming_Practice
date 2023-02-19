53cb04b70652c30e5577521472f56eb8ead5c51f
hoinlee-moi <lhi3048@gmail.com>
2023-02-20 03:30:56 +0900

    add : signUp박스 추가

--- sunflower/src/components/login/LoginBox.tsx
+++ sunflower/src/components/login/LoginBox.tsx
@@ -1,5 +1,30 @@
-const LoginBox = () => {
-    return <></>
-}
+import styles from "../../pages/Login.module.css"
 
-export default LoginBox
+const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_KAKAOLOGIN_API_KEY}&redirect_uri=${process.env.REACT_APP_KAKAOLOGIN_REDIRECT_URI}`;
+
+
+const LoginBox = () => {
+    const kakaoLoginHandle = () => {
+        window.open(KAKAO_AUTH_URL, "카카오 로그인", "width = 400px, height=500px");
+      };
+      
+  return (
+    <div className={styles.loginSection}>
+      <div className={styles.loginBox}>
+        <h4>로그인</h4>
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Password" />
+        <button>로그인</button>
+      </div>
+      <hr />
+      <div className={styles.KakaoLoginBox}>
+        <p>SNS 계정으로 로그인 하기</p>
+        <button onClick={kakaoLoginHandle}>
+          <img src={process.env.PUBLIC_URL + `/assets/kakao_login_btn.png`} />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginBox;
--- sunflower/src/pages/Login.tsx
+++ sunflower/src/pages/Login.tsx
@@ -1,32 +1,20 @@
 import React from "react";
-import styles from './Login.module.css'
 
-const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_KAKAOLOGIN_API_KEY}&redirect_uri=${process.env.REACT_APP_KAKAOLOGIN_REDIRECT_URI}`
+import LoginBox from "../components/login/LoginBox";
+import SignUpBox from "../components/login/SignUpBox";
 
-const Login =() => {
+import styles from "./Login.module.css";
 
-    const kakaoLoginHandle = () =>{
-        window.open(KAKAO_AUTH_URL,"카카오 로그인","width = 400px, height=500px")
-    }
-
-    return <div className={styles.login}>
-        <h3>해바라기</h3>
-        <div className={styles.loginContainer}>
-            <div className={styles.loginSection}>
-            <div className={styles.loginBox}>
-            <h4>로그인</h4>
-            <input type="text" placeholder="Email" />
-            <input type="text" placeholder="Password" />
-            <button>로그인</button>
-            </div>
-            <hr />
-            <div className={styles.KakaoLoginBox}>
-            <p>SNS 계정으로 로그인 하기</p>
-            <button onClick={kakaoLoginHandle}><img src={process.env.PUBLIC_URL + `/assets/kakao_login_btn.png`} /></button>
-            </div>
-            </div>
-        </div>
+const Login = () => {
+  return (
+    <div className={styles.login}>
+      <h3>해바라기</h3>
+      <div className={styles.loginContainer}>
+        <LoginBox />
+        <SignUpBox />
+      </div>
     </div>
-}
+  );
+};
 
-export default Login
+export default Login;
