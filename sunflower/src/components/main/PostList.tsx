9afc12452b62d4fac81b18ff2b5e142cabe397a8
hoinlee-moi <lhi3048@gmail.com>
2023-03-21 04:07:10 +0900

    modify : 무한스크롤과 useObserver 커스텀 훅 새로운 방법으로 변경

--- sunflower/src/components/main/PostList.tsx
+++ sunflower/src/components/main/PostList.tsx
@@ -1,26 +1,28 @@
 import axios, { AxiosHeaders } from "axios";
-import React, { useState, useEffect, useCallback,useRef } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import { useRecoilValue } from "recoil";
 
 import { dummy } from "../../dummy";
 import { getCookie } from "../../etc/Cookie";
-import useObserver from "../../hooks/useObserver";
+import {useIntersectionObserver} from "../../hooks/useObserver";
 import { FloatingMenuWidth } from "../Recoil/RecoilState";
 import PostItemBox from "./PostItemBox";
 import styles from "./PostList.module.css";
 
 const PostList = () => {
   const menuWidth = useRecoilValue(FloatingMenuWidth);
-  const observer = useObserver
   const [postItemList, setPostListItem] = useState(dummy);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  
 
   useEffect(() => {
-    getPostList(page);
+    getPostList();
   }, []);
 
-  const getPostList = async (page: number) => {
+  const getPostList = async () => {
+    console.log(page)
+    setLoading(false);
     await axios
       .get(`/posts/${page}`, {
         headers: {
@@ -29,18 +31,24 @@ const PostList = () => {
       })
       .then((res) => {
         setPostListItem((snap) => [...snap, ...res.data]);
+        setPage(page+1);
       })
-      .catch((err) => console.log(err))
-      .finally(() => setLoading(false));
+      .catch((err) => {
+        console.log(err);
+        setPage(page+1);
+      })
+      .finally(() => {
+        setLoading(true);
+      });
   };
-
+  const setObserver = useIntersectionObserver(getPostList);
   return (
     <div
       className={styles.postListContianer}
       style={{ marginLeft: `${menuWidth}px` }}
     >
       <PostItemBox itemList={postItemList} />
-       <div ref={observerRef}>{loading &&"Loading..."}</div>
+      {loading && <div ref={setObserver}>Loading...</div>}
     </div>
   );
 };
