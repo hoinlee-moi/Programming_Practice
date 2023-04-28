9a22d73c1a4af7c4c7bae829a3c54d27dc1e81d3
hoinlee-moi <lhi3048@gmail.com>
2023-04-29 01:15:14 +0900

    webpack 번들링 성공!

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,5 @@
+// babel.config.js
+
 module.exports = {
-    presets: [
-      ["@babel/preset-react", {"runtime": "automatic"}],
-      "@babel/preset-env",
-      "@babel/preset-typescript",
-    ],
-  };
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+};
