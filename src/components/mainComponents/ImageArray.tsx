1b456a443a1e865e60e131e72bdbe05261e65b35
hoinlee-moi <lhi3048@gmail.com>
2023-05-08 15:52:21 +0900

    add : main페이지 section1 이미지 슬라이드 구현

--- src/components/mainComponents/ImageArray.tsx
+++ src/components/mainComponents/ImageArray.tsx
@@ -1,32 +1,42 @@
-
 type props = {
-    props : string
-}
+  props: string;
+  order: boolean;
+};
 type imgArr = {
-    [key:string] : number[]
-}
-const ImageArray = ({props}:props) => {
-  const imgArr : imgArr = {
-    "a": [0, 1, 2, 0],
-    "b": [3, 4, 4, 4],
-    "c": [5, 6, 6, 6],
-    "d": [6, 6, 6, 6],
-    "e": [7, 8, 8, 8],
-    "f": [8, 9, 9, 9],
-    "g": [10, 11, 11, 11],
+  [key: string]: number[];
+};
+const ImageArray = ({ props, order }: props) => {
+  const imgArr: imgArr = {
+    a: [0, 1, 2, 0],
+    b: [3, 4, 4, 4],
+    c: [5, 6, 6, 6],
+    d: [6, 6, 6, 6],
+    e: [7, 8, 8, 8],
+    f: [8, 9, 9, 9],
+    g: [10, 11, 11, 11],
   };
   return (
     <div>
-      {imgArr[props].map((v, idx) => {
-        return (
-          <img
-            src={`${process.env.PUBLIC_URL}/assets/main/main_food_${v}.jpeg`}
-            key={idx}
-          />
-        );
-      })}
+      {order
+        ? imgArr[props].map((v, idx) => {
+            return (
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/main/main_food_${v}.jpeg`}
+                key={idx}
+              />
+            );
+          })
+        : imgArr[props].map((v, idx) => {
+            return (
+              <img
+                src={`${process.env.PUBLIC_URL}/assets/main/main_info/main_info_${v}.jpg`}
+                key={idx}
+              />
+            );
+          })}
     </div>
   );
 };
 
 export default ImageArray;
+
--- src/components/mainComponents/MainSection01.tsx
+++ src/components/mainComponents/MainSection01.tsx
@@ -5,29 +5,41 @@ import ImageArray from "./ImageArray";
 const MainSection01 = () => {
   const [order, setOrder] = useState(false);
 
-  const imgArr = ["a","b","c","d","e","f","g"];
+  const imgArr = ["a", "b", "c", "d", "e", "f", "g"];
   useEffect(() => {
     setOrder(true);
   }, []);
   useEffect(() => {
-    setTimeout(() => {
-      setOrder(!order);
-    }, 10000);
+    if (!order) {
+      setOrder(true);
+    } else {
+      setTimeout(() => {
+        setOrder(false);
+      }, 16800);
+    }
   }, [order]);
   return (
     <section className={styles.sectionContainer01}>
-      <article className={styles.contentSection}>
-        {order ? (
+      {order && (
+        <article className={styles.contentSection}>
           <p>오늘 하루 식단을 기록해보세요</p>
-        ) : (
           <p>모두와 정보를 공유해보세요</p>
-        )}
-      </article>
-      <article className={styles.pictureSection}>
-        {imgArr.map(value=>{
-          return <ImageArray props={value} />
-        })}
-      </article>
+        </article>
+      )}
+      {order && (
+        <article className={styles.pictureSection}>
+          <div className={styles.picture1}>
+            {imgArr.map((value) => {
+              return <ImageArray props={value} order={true} />;
+            })}
+          </div>
+          <div className={styles.picture2}>
+            {imgArr.map((value) => {
+              return <ImageArray props={value} order={false} />;
+            })}
+          </div>
+        </article>
+      )}
     </section>
   );
 };
